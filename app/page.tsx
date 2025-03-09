0d34ef7e2e0eaeed3d00e7e68a52e756e65cfa44
zethboy <yoginugraha842@gmail.com>
2025-03-09 15:21:53 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,132 @@
-import Image from "next/image";
 
-export default function Home() {
+"use client";
+
+import { resolve } from "path";
+import { useRef, useState } from "react" 
+import Stream from "stream";
+
+export default function photoBooth () {
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const startCamera = async () => {
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({video:true})
+      console.log("ref video", videoRef.current)
+      
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream as MediaStream
+        videoRef.current.play();
+      }
+    } catch(err){
+      console.error("gagal mengakses kamera :", err)
+    }
+  }
+
+  const [photos, setPhotos] = useState<string[]>([])
+
+  const capturePhoto = () => {
+      if(!videoRef.current) return
+      const canvas = document.createElement("canvas")
+      canvas.width = videoRef.current.videoWidth
+      canvas.height = videoRef.current.videoHeight
+      const ctx = canvas.getContext("2d")
+      if(ctx){
+        ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
+        const imageUrl = canvas.toDataURL("image/png")
+
+        setPhotos((prev) => (prev.length < 3 ? [...prev, imageUrl] : prev))
+      }
+   }
+
+  const [finalPhoto, setFinalPhoto] = useState<string | null>(null)
+
+  const combinePhotos = async () => {
+
+    console.log("proses bosku")
+
+    if(photos.length < 3) {
+      alert ("ambil mminimal 3 bosku")
+      return
+    }
+
+    console.log("poto cukup")
+    const canvas = document.createElement("canvas")
+    const borderSize = 0
+    const imgWidth = 300
+    const imgHeight = 200
+    canvas.width =  imgWidth * 3 + borderSize * 4
+    canvas.height = imgHeight  + borderSize * 2
+    const ctx = canvas.getContext("2d")
+    
+
+    if (!ctx){
+      console.log("gadapet context canvas")
+      return
+    } 
+    console.log("canvas aman")
+    const loadImage = (src : string) => {
+      return new Promise<HTMLImageElement>((resolve)=>{
+        const img = new Image()
+        img.src = src;
+        img.onload = () =>{
+          console.log("gambar dimuat")
+          resolve(img)}
+          img.onerror = (err) => {
+            console.error("❌ Gagal memuat gambar:", src, err);
+          };
+       }
+      )
+       
+      
+    }
+
+try {
+  console.log("muat gambar")
+    const images = await Promise.all(photos.map(loadImage)); 
+    console.log("udah dimuat")   
+
+    const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+
+
+    images.forEach((image, index) => {
+      const borderColor = colors[index % colors.length]; // Pilih warna dari array
+      ctx.fillStyle = borderColor;
+      ctx.fillRect(index * imgWidth, 0, imgWidth, imgHeight); 
+      ctx.drawImage(image, index * imgWidth + 5, 5, imgWidth - 10, imgHeight - 10);
+      console.log(`✅ Gambar ${index + 1} ditambahkan ke canvas!`)   
+    });
+
+    // Simpan hasilnya
+    const finalImage = canvas.toDataURL("image/png");
+    setFinalPhoto(finalImage);
+  } catch (error) {
+    console.error("Gagal memuat gambar:", error);
+}
+}
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
+      <h1 className="text-3xl font-bold mb-4" >Giogi PhotoBooth</h1>
+      <video ref={videoRef} id="video" style={{transform: "scaleX(-1)"}} autoPlay playsInline className="border-white border-2 h-auto w-full max-w-md"></video>
+      <button onClick={startCamera} className="mt-4 bg-green-500 px-4 py-2 rounded-lg text-white hover:bg-green-400">Aktifkan Kamera</button>
+      <button onClick={capturePhoto} className="bg-blue-600 text-white px-4 py-2 mt-2 rounded">Ambil foto</button>
+      <button onClick={combinePhotos} className="bg-purple-600 px-4 py-2 mt-2 rounded">Gabungkan foto</button>
+      
+      {finalPhoto && (
+        <div className="mt-4">
+          <h2 className="text-white">Hasil</h2>
+          <img src={finalPhoto} alt="gabungan foto" className="" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      
+      <div className="flex gap-2 mt-4">
+        {photos.map((photo, index)=>(
+          <img key={index} src={photo} alt={`Foto ${index}`} className="w-32 h-32 object-cover border" />
+        ))}
+      </div>
+
     </div>
-  );
+  )
 }
